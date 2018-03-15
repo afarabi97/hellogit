bf7de8612d849c478e059870ee577dd97018995c
prodian0013 <micah.downing@mantech.com>
2018-03-15 13:58:22 -0500

    base size reduced and updated icdm

---
+++ base/docker/icdm.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+import argparse
+import getopt
+import os
+import os.path
+import socket
+import sys
+
+import yaml
+from jinja2 import DebugUndefined, Environment, FileSystemLoader, Template
+
+
+def render(tpl_path, context):
+    path, filename = os.path.split(tpl_path)
+    return Environment(
+        loader=FileSystemLoader(path or './')
+    ).get_template(filename).render(context)
+
+
+def process(icdm_dir, icdm_vars):
+    for root, directories, filenames in os.walk(icdm_dir):
+        for filename in filenames:
+            if filename.endswith(".conf"):
+                configvars = {}
+                with open(os.path.join(root, filename)) as configfile:
+                    for line in configfile:
+                        name, var = line.partition(":")[::2]
+                        configvars[name.strip()] = var.rstrip("\n\r").strip()
+                if os.path.isfile(configvars['src']):
+                    with open(icdm_vars) as simple:
+                        simple = yaml.load(simple)
+
+                    if simple is None:
+                        raise Exception('icdm vars is empty')
+
+                    if 'HOSTNAME' in os.environ:
+                        hostname = os.environ['HOSTNAME']
+                    elif len(socket.gethostname()) > 1:
+                        hostname = socket.gethostname()
+                    else:
+                        hostname = "localhost"
+
+                    simple['host'] = hostname
+
+                    with open(configvars['dst'], 'wb') as fh:
+                        fh.write(render(configvars['src'], simple))
+
+                    os.chown(configvars['dst'], 0, 0)
+                    os.chmod(configvars['dst'], 0644)
+
+
+def usage():
+    print 'Usage: icdm.py -d <icdm dir> -v <icdm vars>'
+
+
+if __name__ == "__main__":
+    icdm_dir = '/etc/icdm'
+    icdm_vars = '/etc/icdm/vars.yml'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dir", help="icdm dir")
+    parser.add_argument("-v", "--vars", help="icdm vars file")
+    args = parser.parse_args()
+
+    if args.dir:
+        icdm_dir = args.dir
+    if args.vars:
+        icdm_vars = args.vars
+
+    if not os.path.isdir(icdm_dir):
+        raise Exception('icdm dir not found')
+    if not os.path.isfile(icdm_vars):
+        raise Exception('icdm vars not found')
+
+    process(icdm_dir, icdm_vars)
--- base/docker/icdm.sh
+++ base/docker/icdm.sh
@@ -1,33 +1,19 @@
 #!/bin/bash
 
+icdm_dir=/etc/icdm
+icdm_vars=/etc/icdm/vars.yml
+
 parse_templates () {
-  for conffile in $(find /etc/icdm -type l -name "*.conf"); do
-    src=$(cat $conffile | grep -P '^src:\s+' | head -n 1 | awk '{ $1=""; print $0}')
-    dst=$(cat $conffile | grep -P '^dst:\s+' | head -n 1 | awk '{ $1=""; print $0}')
-    uid=$(cat $conffile | grep -P '^uid:\s+' | head -n 1 | awk '{ $1=""; print $0}')
-    gid=$(cat $conffile | grep -P '^gid:\s+' | head -n 1 | awk '{ $1=""; print $0}')
-    mode=$(cat $conffile | grep -P '^mode:\s+' | head -n 1 | awk '{ $1=""; print $0}')
-    reload_cmd=$(cat $conffile | grep -P '^reload_cmd:\s+' | head -n 1 | awk '{ $1=""; print $0}')    
-    # Remove trailing \r
-    src=`echo $src | sed 's/\\r//g'`
-    dst=`echo $dst | sed 's/\\r//g'`
-    uid=`echo $uid | sed 's/\\r//g'`
-    gid=`echo $gid | sed 's/\\r//g'`
-    mode=`echo $mode | sed 's/\\r//g'`
-    reload_cmd=`echo $reload_cmd | sed 's/\\r//g'`
-    if jinja2 --format=yml $src /etc/icdm/vars.yml -D host=$HOSTNAME > /tmp/$(basename $src); then
-      cp /tmp/$(basename $src) $dst
-      chown $uid:$gid $dst
-      chmod $mode $dst
-      bash -c "$reload_cmd"
-      rm /tmp/$(basename $src)
-    fi
-  done;
+  /usr/bin/python /icdm.py -d $icdm_dir -v $icdm_vars
+  ret=$?
+  if [ $ret -ne 0 ]; then
+    exit 1
+  fi
 }
 
 parse_templates
 
-while /bin/inotifywait -r -e modify,create,delete /etc/icdm;
+while /bin/inotifywait -r -e modify,create,delete $icdm_dir;
 do
   parse_templates
 done;
