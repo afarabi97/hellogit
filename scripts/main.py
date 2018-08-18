a2bcd419bfe39f1b606cdae2d201702cb84d2523
Grant Curell <grant.curell@salientcrgt.com>
2018-08-18 11:09:26 -0500

    Working on it

--- scripts/main.py
+++ scripts/main.py
@@ -3,22 +3,34 @@
 __author__ = 'Grant Curell'
 __vcenter_version__ = '6.7c'
 
-"""
-from com.vmware.vcenter.vm.hardware.boot_client import Device as BootDevice
-from com.vmware.vcenter.vm.hardware_client import (
-    Cpu, Memory, Disk, Ethernet, Cdrom, Serial, Parallel, Floppy, Boot)
-from com.vmware.vcenter.vm.hardware_client import ScsiAddressSpec
-from com.vmware.vcenter.vm_client import (Hardware, Power)
-from com.vmware.vcenter_client import VM
-"""
-
+import yaml
 import requests
 import urllib3
 from vmware.vapi.vsphere.client import create_vsphere_client
+from VM import Virtual_Machine
 
-from VM import Deployer_VM
+def create_vms(configuration: dict):
+    """
+    Creates the VMs specified in the VMs.yml file on the chosen target VMWare devices
+
+    :param configuration (dict): A YAML file defining the schema of the kit
+    :return:
+    """
+    #print(yaml.dump(configuration))
+
+    #for vm, attributes in configuration["VMs"]["test_controller"]:
+    #    print("balls")
+
+    for vm in configuration["VMs"].keys():
+        print(configuration["VMs"][vm]["type"])
 
 def main():
+
+    with open("VMs.yml", 'r') as kit_schema:
+        try:
+            configuration = yaml.load(kit_schema)
+        except yaml.YAMLError as exc:
+            print(exc)
 
     session = requests.session()
 
@@ -31,11 +43,21 @@ def main():
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
     # Connect to a vCenter Server using username and password
-    vsphere_client = create_vsphere_client(server='172.16.70.5',
-    username='test', password='test', session=session)
+
+    vsphere_client = create_vsphere_client(
+    server=configuration["host_configuration"]["vcenter"]["ip_address"],
+    username=configuration["host_configuration"]["vcenter"]["username"],
+    password=configuration["host_configuration"]["vcenter"]["password"],
+    session=session)
+    
+    print(configuration["host_configuration"]["vcenter"]["ip_address"])
+
+    print(type(vsphere_client))
 
     # List all VMs inside the vCenter Server
     #print(vsphere_client.vcenter.VM.list())
+    """
+    create_vms(configuration)
 
     create_deployer_vm = Deployer_VM(client=vsphere_client)
     create_deployer_vm.cleanup()
@@ -43,7 +65,7 @@ def main():
     if create_deployer_vm.cleardata:
         create_deployer_vm.cleanup()
     create_deployer_vm.power_on()
-
+    """
 
 if __name__ == '__main__':
     main()
