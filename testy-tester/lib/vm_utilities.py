a5a75d91a26a0a37c21b01e9c7875fcf0db5abee
Grant Curell <grant.curell@salientcrgt.com>
2018-08-23 16:45:28 -0500

    Fixes

--- testy-tester/lib/vm_utilities.py
+++ testy-tester/lib/vm_utilities.py
@@ -1,144 +1,37 @@
-#!/usr/bin/env python
-"""
-Written by Dann Bohn
-Github: https://github.com/whereismyjetpack
-Email: dannbohn@gmail.com
-Clone a VM from template example
-"""
 from pyVmomi import vim
 from pyVim.connect import SmartConnect, Disconnect
-import atexit
+import vmutils
 
-def wait_for_task(task):
-    """ wait for a vCenter task to finish """
-    task_done = False
-    while not task_done:
-        if task.info.state == 'success':
-            return task.info.result
+def clone_vm(configuration: OrderedDict) -> list:
+    username = configuration["host_configuration"]["vcenter"]["username"]
+    password = configuration["host_configuration"]["vcenter"]["password"]
+    vcenter_ip = configuration["host_configuration"]["vcenter"]["ip_address"]
+    vcenter_port = '10504'
+    cluster_name = 'Management'
+    template_name = 'Windows2008R2-Template'
+    customization_spec_name = 'Windows2008Prep'
+    new_vm_name = 'MyClonedWin2008'
 
-        if task.info.state == 'error':
-            print "there was an error"
-            task_done = True
+    # This will connect us to vCenter
+    s = SmartConnect(host=vcenter_ip, user=username, pwd=password, port=vcenter_port)
 
+    # With this we are searching for the MOID of the VM to clone from
+    template_vm = vmutils.get_vm_by_name(s, template_name)
 
-def get_obj(content, vimtype, name):
-    """
-    Return an object by name, if name is None the
-    first found object is returned
-    """
-    obj = None
-    container = content.viewManager.CreateContainerView(
-        content.rootFolder, vimtype, True)
-    for c in container.view:
-        if name:
-            if c.name == name:
-                obj = c
-                break
-        else:
-            obj = c
-            break
+    # This gets the MOID of the Guest Customization Spec that is saved in the vCenter DB
+    guest_customization_spec = s.content.customizationSpecManager.GetCustomizationSpec(name=customization_spec_name)
 
-    return obj
+    # This will retrieve the Cluster MOID
+    cluster = vmutils.get_cluster(s, cluster_name)
 
+    # This constructs the reloacate spec needed in a later step by specifying the default resource pool (name=Resource) of the Cluster
+    # Alternatively one can specify a custom resource pool inside of a Cluster
+    relocate_spec = vim.vm.RelocateSpec(pool=cluster.resourcePool)
 
-def clone_vm(
-        content, template, vm_name, si,
-        datacenter_name, vm_folder, datastore_name,
-        cluster_name, resource_pool, power_on, datastorecluster_name):
-    """
-    Clone a VM from a template/VM, datacenter_name, vm_folder, datastore_name
-    cluster_name, resource_pool, and power_on are all optional.
-    """
+    # This constructs the clone specification and adds the customization spec and location spec to it
+    cloneSpec = vim.vm.CloneSpec(powerOn=True, template=False, location=relocate_spec, customization=guest_customization_spec.spec)
 
-    # if none git the first one
-    datacenter = get_obj(content, [vim.Datacenter], datacenter_name)
+    # Finally this is the clone operation with the relevant specs attached
+    clone = template_vm.Clone(name=new_vm_name, folder=template_vm.parent, spec=cloneSpec)
 
-    if vm_folder:
-        destfolder = get_obj(content, [vim.Folder], vm_folder)
-    else:
-        destfolder = datacenter.vmFolder
-
-    if datastore_name:
-        datastore = get_obj(content, [vim.Datastore], datastore_name)
-    else:
-        datastore = get_obj(
-            content, [vim.Datastore], template.datastore[0].info.name)
-
-    # if None, get the first one
-    cluster = get_obj(content, [vim.ClusterComputeResource], cluster_name)
-
-    if resource_pool:
-        resource_pool = get_obj(content, [vim.ResourcePool], resource_pool)
-    else:
-        resource_pool = cluster.resourcePool
-
-    vmconf = vim.vm.ConfigSpec()
-
-    if datastorecluster_name:
-        podsel = vim.storageDrs.PodSelectionSpec()
-        pod = get_obj(content, [vim.StoragePod], datastorecluster_name)
-        podsel.storagePod = pod
-
-        storagespec = vim.storageDrs.StoragePlacementSpec()
-        storagespec.podSelectionSpec = podsel
-        storagespec.type = 'create'
-        storagespec.folder = destfolder
-        storagespec.resourcePool = resource_pool
-        storagespec.configSpec = vmconf
-
-        try:
-            rec = content.storageResourceManager.RecommendDatastores(
-                storageSpec=storagespec)
-            rec_action = rec.recommendations[0].action[0]
-            real_datastore_name = rec_action.destination.name
-        except:
-            real_datastore_name = template.datastore[0].info.name
-
-        datastore = get_obj(content, [vim.Datastore], real_datastore_name)
-
-    # set relospec
-    relospec = vim.vm.RelocateSpec()
-    relospec.datastore = datastore
-    relospec.pool = resource_pool
-
-    clonespec = vim.vm.CloneSpec()
-    clonespec.location = relospec
-    clonespec.powerOn = power_on
-
-    print "cloning VM..."
-    task = template.Clone(folder=destfolder, name=vm_name, spec=clonespec)
-    wait_for_task(task)
-
-
-def main():
-    """
-    Let this thing fly
-    """
-
-    # connect this thing
-    si = SmartConnect(
-        host=args.host,
-        user=args.user,
-        pwd=args.password,
-        port=args.port)
-    # disconnect this thing
-    atexit.register(Disconnect, si)
-
-    content = si.RetrieveContent()
-    template = None
-
-    template = get_obj(content, [vim.VirtualMachine], args.template)
-
-    if template:
-        clone_vm(
-            content, template, args.vm_name, si,
-            args.datacenter_name, args.vm_folder,
-            args.datastore_name, args.cluster_name,
-            args.resource_pool, args.power_on, args.datastorecluster_name)
-    else:
-        print "template not found"
-
-
-# start this thing
-if __name__ == "__main__":
-    main()
+    Disconnect(s)
