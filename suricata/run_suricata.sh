d0b6e423be148e72c4a4166b5c258c25ce30c0ae
Grant Curell <grant.curell@salientcrgt.com>
2019-01-31 12:06:26 -0600

    Dashboard update
    Former-commit-id: 7ac6c2b2bfc148ba50b65ea89d908dee3adff686

--- suricata/run_suricata.sh
+++ suricata/run_suricata.sh
@@ -1,5 +1,12 @@
-#1/bin/bash
+#!/bin/bash
 
-/usr/share/filebeat/bin/filebeat -c /etc/filebeat/filebeat.yml -path.home /usr/share/filebeat -path.config /etc/filebeat -path.data /var/lib/filebeat -path.logs /var/log/filebeat &
-
-/usr/sbin/suricata -c /etc/suricata/suricata.yaml --af-packet
+if [[ ! -z "${PODTYPE}" ]]; then  
+  if [[ "${PODTYPE}" == "SURICATA" ]]; then
+    filebeat modules enable suricata
+    /usr/share/filebeat/bin/filebeat -c /etc/filebeat/filebeat.yml -path.home /usr/share/filebeat -path.config /etc/filebeat -path.data /var/lib/filebeat -path.logs /var/log/filebeat &
+    /usr/sbin/suricata -c /etc/suricata/suricata.yaml --af-packet
+  elif [[ "${PODTYPE}" == "BOOTSTRAP" ]]; then
+    filebeat modules enable suricata
+    filebeat setup -e
+  fi
+fi
