66182bd3e127ff11afea28290c3752f04ca9869c
David Navarro <david.navarro@mantech.com>
2019-03-27 16:33:48 -0500

    2373 - Upgraded suricata from version 4.0.6 to 4.1.3.

---
+++ playbooks/roles/suricata/files/script1.lua
@@ -0,0 +1,15 @@
+function init (args)
+    local needs = {}
+    needs["protocol"] = "http"
+    return needs
+end
+
+function setup (args)
+end
+
+function log(args)
+end
+
+function deinit (args)
+ -- Test
+end
