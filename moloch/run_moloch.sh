3eab1a9c83a7c59a4cec0b5cb7d37a36a89d4738
Prodian <micah.downing@gmail.com>
2019-04-26 12:49:55 -0500

    add ulimit to moloch run script
    Former-commit-id: 2cb1d4457e5841890b39aa96e738dfb3080f1803

--- moloch/run_moloch.sh
+++ moloch/run_moloch.sh
@@ -5,6 +5,8 @@
 # value True to  those pods meant for capture and False for the viewer pod.
 # This allows us to only use one container for both the viewer and capture pods
 
+ulimit -l unlimited
+
 if [[ ! -z "${PODTYPE}" ]]; then  
   if [[ "${PODTYPE}" == "CAPTURE" ]]; then    
     cd /data/moloch/viewer
@@ -13,7 +15,7 @@ if [[ ! -z "${PODTYPE}" ]]; then
   elif [[ "${PODTYPE}" == "VIEWER" ]]; then
     cd /data/moloch/viewer
     /data/moloch/bin/node /data/moloch/viewer/viewer.js -c /data/moloch/etc/config.ini
-  elif [[ "${PODTYPE}" == "BOOTSTRAP" ]]; then
+  elif [[ "${PODTYPE}" == "BOOTSTRAP" ]]; then    
     /usr/bin/expect /data/moloch/bin/auto-init
     /data/moloch/bin/moloch_add_user.sh -c /data/moloch/etc/config.ini "${MOLOCH_LOGIN}" "${MOLOCH_LOGIN}" "${MOLOCH_PASS}" --admin --webauth
   fi
