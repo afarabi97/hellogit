6d2a185d5b79888b1a51273393e03ba50669ccac
Matthew Duplantis <matthew.duplantis@salientcrgt.com>
2019-06-05 13:34:37 -0500

    THISISCVAH-2711
    chart.py rework

--- deployer/playbooks/roles/bootstrap/files/chart.py
+++ deployer/playbooks/roles/bootstrap/files/chart.py
@@ -1,48 +1,114 @@
 import os
+import re
 import sys
 import yaml
 import docker
+import logging
+import os.path
 import tarfile
+import subprocess
+from os import path
 from os import listdir
 from os.path import isfile, join
-
-def get_image(chart):
-    tar = dir + chart
-    tar = tarfile.open(tar)
-    for member in tar.getmembers():
-        if member.isreg() and "values.yaml" in member.name:
-            f = tar.extractfile(member)
-            stream = f.read()
-            data = yaml.load(stream)
-            if "image" in data and "tag" in data["image"] and "repository" in data["image"]:
-              tag = data["image"]["tag"]
-              repo = data["image"]["repository"]
-              if "registry" in data["image"]:
-                  registry = data["image"]["registry"]
-              else:
-                  registry = "docker.io"
-              return [registry, repo, tag]
-            else:
-              print("skipping " + chart +"! Chart does not conform to standards!")
-              continue
+from pyhelm.chartbuilder import ChartBuilder
 
 def get_charts(dir):
     files = [f for f in listdir(dir) if isfile(join(dir, f))]
     return files
 
+def get_chart_name(members):
+    for member in members:
+        if member.isreg() and "Chart.yaml" in member.name:
+            f = tarfile.open(tar)
+            f = f.extractfile(member)
+            stream = f.read()
+            data = yaml.load(stream)
+            name = data["name"]
+            f.close()
+            return name
+
+def get_tar_members(tar):
+    f = tarfile.open(tar)
+    members = f.getmembers()
+    f.close()
+    return members
+
+def extract_chart(tar):
+    f = tarfile.open(tar)
+    f.extractall(path="charts")
+    f.close()
+
 dir = sys.argv[1]
 client = docker.from_env()
 local_registry = "localhost:5000"
-
 charts = get_charts(dir)
+
+if not os.path.exists("executed_charts"):
+    print("creating chart path")
+    os.mkdir("executed_charts")
+else:
+    print("Chart path already exists")
+
 for chart in charts:
-    image = get_image(chart)
-    if image:
-      print("Pulling " + image[0] + "/" + image[1] + ":" + image[2])
-      client.images.pull(image[0] + "/" + image[1] + ":" + image[2])
-      img = client.images.get(image[1] + ":" + image[2])
-      print("Tagging " + local_registry + "/" + image[1] + ":" + image[2])
-      img.tag(local_registry + "/" + image[1] + ":" + image[2])
-      push_img = local_registry + "/" + image[1] + ":" + image[2]
-      print("pushing " + push_img)
-      client.images.push(push_img)
+    tar = dir + chart
+    members = get_tar_members(tar)
+    chart_name = get_chart_name(members)
+    extract_chart(tar)
+    files = os.listdir("charts/" + chart_name + "/templates")
+    for file in files:
+        path = "charts/" + chart_name + "/templates/" + file
+        if os.path.isfile(path):
+            f = open(path)
+            contents = f.readlines()
+            for line in contents:
+                if "kind: Deployment" in line or "kind: Statefulset" in line or "kind: Daemonset" in line:
+                    subprocess.call(['/usr/local/bin/helm', 'template', 'charts/' + chart_name, '--debug', '--output-dir=./executed_charts', '-x' + 'templates/' + file])
+
+images = []
+for root, directories, filenames in os.walk('executed_charts/'):
+    for filename in filenames:
+        path = os.path.join(root,filename)
+        f = open(path)
+        contents = f.readlines()
+        for line in contents:
+            if "image:" in line:
+                a = line.strip()
+                pat = re.compile("image:.*")
+                res = pat.findall(a)
+                s = res[0]
+                s = s.split(" ")
+                s = s[1]
+                s = s.strip('\"')
+                s = s.strip("\'")
+                if s != ":":
+                    if s not in images:
+                        images.append(s)
+
+for image in images:
+    if ":" not in image:
+        tag = "latest"
+    if ":" in image:
+        repo = image.split(":")[0]
+        tag = image.split(":")[1]
+        pat = re.compile("^[^/]*")
+        res = pat.match(repo)
+        if "." in res.group(0):
+            registry = res.group(0)
+            repo = str(repo).split("/",1)[1]
+        else:
+            registry = "docker.io"
+        try:
+            print("Pulling " + registry + "/" + repo + ":" + tag)
+            client.images.pull(registry + "/" + repo,  tag=tag)
+            if "." in res.group(0):
+                img = client.images.get(registry + "/" + repo + ":" + tag)
+            else:
+                img = client.images.get(repo + ":" + tag)
+            print("Tagging " + local_registry + "/" + repo + ":" + tag)
+            img.tag(local_registry + "/" + repo + ":" + tag)
+            push_img = local_registry + "/" + repo + ":" + tag
+            print("pushing " + push_img)
+            client.images.push(push_img)
+        except:
+            print("Skipping " + registry + "/" + repo + ":" + tag)
+            pass
