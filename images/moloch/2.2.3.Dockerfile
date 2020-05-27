46b8b92bbdcee532fc3fb5b8c236ceef5a6e16e4
Dylan Sigler <dylan.sigler@bylight.com>
2020-05-27 14:06:19 -0500

    5897 - Added ILM policy to moloch image
    Added vars to moloch ilm commands
    Changed moloch image version from 2.2.3 to 2.2.3v2 which includes the ilm policies
    Removed old moloch-custom policy
    Added logic for number of ilm shards based on kit size and added more vars for moloch ilm
    Change archive pulls from web to labrepo. Labrpo pulls archives nightly. This is so we don't have to use api key here. Pushed new docker image tfplenum/moloch:2.2.3v2
    5897 - Removed extra var in moloch docker image and modified number of shards based on kit size.
    5897 - removed commented out code and deleted the old template used for moloch ilm.
    Changed delete policy from 90d to 60d
    Removed custom index policies

--- images/moloch/2.2.3.Dockerfile
+++ images/moloch/2.2.3.Dockerfile
@@ -1,12 +1,10 @@
 FROM centos:7
 
-ARG maxmind_api_key
-ENV api_key=${maxmind_api_key}
 COPY run_moloch.sh /data/moloch/bin/
 
 RUN yum install -y epel-release && yum install -y https://files.molo.ch/builds/centos-7/moloch-2.2.3-1.x86_64.rpm && yum clean all && \
-    curl -L -o /tmp/GeoLite2-Country.tar.gz "https://download.maxmind.com/app/geoip_download?license_key=${api_key}&edition_id=GeoLite2-Country&suffix=tar.gz" && \
-    curl -L -o /tmp/GeoLite2-ASN.tar.gz "https://download.maxmind.com/app/geoip_download?license_key=${api_key}&edition_id=GeoLite2-ASN&suffix=tar.gz" && \
+    curl -o /tmp/GeoLite2-Country.tar.gz "http://misc.labrepo.sil.lab/GeoLite2-Country.tar.gz" && \
+    curl -o /tmp/GeoLite2-ASN.tar.gz "http://misc.labrepo.sil.lab/GeoLite2-ASN.tar.gz" && \
     curl -L -o /data/moloch/etc/ipv4-address-space.csv "https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.csv" && \
     curl -L -o /data/moloch/etc/oui.txt "https://raw.githubusercontent.com/wireshark/wireshark/master/manuf" && \
     tar -ztf /tmp/GeoLite2-Country.tar.gz | grep mmdb | xargs -I X tar -Ozxf /tmp/GeoLite2-Country.tar.gz X >> /data/moloch/etc/GeoLite2-Country.mmdb && \
--- images/moloch/run_moloch.sh
+++ images/moloch/run_moloch.sh
@@ -12,6 +12,8 @@ if [[ ! -z "${CONTAINER_TYPE}" ]]; then
     /data/moloch/bin/node /data/moloch/viewer/addUser.js --insecure -c /data/moloch/etc/config.ini ${MOLOCH_USER} ${MOLOCH_USER} ${MOLOCH_PASSWORD} --admin --webauth
     /data/moloch/bin/node /data/moloch/viewer/viewer.js -c /data/moloch/etc/config.ini
   elif [[ "${CONTAINER_TYPE}" == "BOOTSTRAP" ]]; then
-    /data/moloch/db/db.pl --insecure https://${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}@${ELASTICSEARCH_FQDN}:${ELASTICSEARCH_PORT} initnoprompt --replicas ${NUM_OF_REPLICAS} --shardsPerNode ${SHARDS_PER_NODE}
+    /data/moloch/db/db.pl --insecure https://${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}@${ELASTICSEARCH_FQDN}:${ELASTICSEARCH_PORT} initnoprompt --shards ${NUM_OF_SHARDS} --replicas ${NUM_OF_REPLICAS} --shardsPerNode ${SHARDS_PER_NODE}
+    /data/moloch/db/db.pl --insecure https://${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}@${ELASTICSEARCH_FQDN}:${ELASTICSEARCH_PORT} ilm ${FORCE_MERGE} ${DELETE} --history ${HISTORY} --replicas ${ILM_REPLICAS}
+    /data/moloch/db/db.pl --insecure https://${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}@${ELASTICSEARCH_FQDN}:${ELASTICSEARCH_PORT} upgradenoprompt --shards ${NUM_OF_SHARDS} --replicas ${NUM_OF_REPLICAS} --shardsPerNode ${SHARDS_PER_NODE} --ilm
   fi
 fi
