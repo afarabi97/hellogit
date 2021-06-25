2820cd0e12616b94ac57bf7bf4b6cf46aa9082f5
Kassio Coutinho <kassio.coutinho@mantech.com>
2021-06-25 08:40:45 -0500

    Finding: THISISCVAH-10288
    improved clear network interface before export

--- testing/pipeline/scripts/reset_system.sh
+++ testing/pipeline/scripts/reset_system.sh
@@ -92,10 +92,24 @@ function reset_password {
 }
 
 function update_network_scripts {
-    sed -i 's/^\(BOOTPROTO=\).*/\1dhcp/g' /etc/sysconfig/network-scripts/ifcfg-br0
-    sed -i '/^IPADDR/ d' /etc/sysconfig/network-scripts/ifcfg-br0
-    sed -i '/^GATEWAY/ d' /etc/sysconfig/network-scripts/ifcfg-br0
-    sed -i '/^DNS1/ d' /etc/sysconfig/network-scripts/ifcfg-br0
+    ENS192=/etc/sysconfig/network-scripts/ifcfg-ens192
+    BR0=/etc/sysconfig/network-scripts/ifcfg-br0
+
+    if [ -f "$ENS192" ]; then
+        sed -i 's/^\(BOOTPROTO=\).*/\1dhcp/g' /etc/sysconfig/network-scripts/ifcfg-ens192
+        sed -i '/^IPADDR/ d' /etc/sysconfig/network-scripts/ifcfg-ens192
+        sed -i '/^GATEWAY/ d' /etc/sysconfig/network-scripts/ifcfg-ens192
+        sed -i '/^DNS1/ d' /etc/sysconfig/network-scripts/ifcfg-ens192
+    fi
+
+    if [ -f "$BR0" ]; then
+        sed -i 's/^\(BOOTPROTO=\).*/\1dhcp/g' /etc/sysconfig/network-scripts/ifcfg-br0
+        sed -i '/^IPADDR/ d' /etc/sysconfig/network-scripts/ifcfg-br0
+        sed -i '/^GATEWAY/ d' /etc/sysconfig/network-scripts/ifcfg-br0
+        sed -i '/^DNS1/ d' /etc/sysconfig/network-scripts/ifcfg-br0
+    fi
+
+    nmcli conn delete id 'Wired connection 1' 2>/dev/null
     nmcli conn reload
 }
 
