e77d23718ec963cf2758c01822b5b3dacf2cde49
David Navarro <david.navarro@mantech.com>
2022-03-28 18:54:59 -0400

    finding/THISISCVAH-12365 - Firewall and Switch fixes
    Removed STIG that was breaking remote sensor.
    Added route for finding/THISISCVAH-12364 per Brian Ermovick.
    Fix other misc issues with switch and firewall.

--- component-builder/cleanup-nexus.sh
+++ component-builder/cleanup-nexus.sh
@@ -3,13 +3,18 @@ ALL_COMPONENTS=$(ls components)
 NEXUS_USER="tfplenum-bot"
 NEXUS_PASSWORD=""
 RPM_VERSION="3.7.0"
-RPM_RELEASE_NUM="11"
+RPM_RELEASE_NUM="19"
 REGISTRY_URL="https://nexus.sil.lab/repository/tfplenum-stable"
 
-for component in $ALL_COMPONENTS
+for component in $ALL_COMPONENTS repo
 do
     binary_name=tfplenum-$component-$RPM_VERSION-$RPM_RELEASE_NUM.el8.x86_64.rpm
     cmd="curl -I --request DELETE --user \"$NEXUS_USER:$NEXUS_PASSWORD\" \"$REGISTRY_URL/$binary_name\""
     echo $cmd
     eval $cmd
 done
+
+binary_name=tfplenum-$RPM_VERSION-$RPM_RELEASE_NUM.el8.x86_64.rpm
+cmd="curl -I --request DELETE --user \"$NEXUS_USER:$NEXUS_PASSWORD\" \"$REGISTRY_URL/$binary_name\""
+echo $cmd
+eval $cmd
