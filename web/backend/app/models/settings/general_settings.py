3f599bce55e066d6bba1d834be5a00c2a6112ac3
jacob.dorlac <jaocb.dorlac@sil.lab>
2022-04-21 12:09:19 -0500

    (fix) finding/THISISCVAH-12498
    - part 2

--- web/backend/app/models/settings/general_settings.py
+++ web/backend/app/models/settings/general_settings.py
@@ -47,9 +47,9 @@ class GeneralSettingsSchema(Schema):
 
     @validates("domain")
     def validate_domain(self, value: str):
-        pattern = "^[a-z]([a-z0-9-]){2,51}$"
+        pattern = f"^[a-z]([a-z0-9-]){2,39}$"
         if not re.match(pattern, value):
-            raise ValidationError("Domain must be alphanumeric, less than 51 characters. "
+            raise ValidationError("Domain must be alphanumeric, less than 40 characters. "
                                   "Special characters are not allowed with the exception of dashes (IE -).")
 
 
--- web/frontend/src/app/system-setupv2/validators/kit-setup-validators.ts
+++ web/frontend/src/app/system-setupv2/validators/kit-setup-validators.ts
@@ -104,7 +104,7 @@ export const kitSettingsValidators = {
   upstream_dns: [{ ops: { pattern: new RegExp(IP_CONSTRAINT) }, error_message: 'You must enter a valid IP address.', validatorFn: 'pattern' }],
   upstream_ntp: [{ ops: { pattern: new RegExp(IP_CONSTRAINT) }, error_message: 'You must enter a valid IP address.', validatorFn: 'pattern' }],
   domain: [
-    { ops: { pattern: /^[a-z]([a-z0-9-]){2,51}$/ }, error_message: 'Domain must be alphanumeric and 52 characters or less. Special characters are not allowed with the exception of dashes (IE -).', validatorFn: 'pattern' },
+    { ops: { pattern: /^[a-z]([a-z0-9-]){2,39}$/ }, error_message: 'Domain must be alphanumeric, less than 40 characters. Special characters are not allowed with the exception of dashes (IE -).', validatorFn: 'pattern' },
     { error_message: 'Domain is required', validatorFn: 'required' }
   ],
   kubernetes_services_cidr: [
@@ -115,7 +115,7 @@ export const kitSettingsValidators = {
 export const addNodeValidators = {
   hostname: [
     { error_message: (value) => `Duplicate hostnames found: ${value}. Node must have a unique hostnames.`, validatorFn: 'unique' },
-    { ops: { pattern: /^[a-z]([a-z0-9-.]){4,51}$/ }, error_message: 'Hostname must be alphanumeric with a length between 5 and 52 characters. Special characters are not allowed with the exception of dashes (IE -).', validatorFn: 'pattern' },
+    { ops: { pattern: /^[a-z]([a-z0-9-.]){4,39}$/ }, error_message: 'Hostname must be alphanumeric, less than 40 characters. Special characters are not allowed with the exception of dashes (IE -).', validatorFn: 'pattern' },
     { error_message: 'Hostname is required', validatorFn: 'required' }
   ],
   ip_address: [
@@ -168,7 +168,7 @@ export const addNodeValidators = {
 
 export const kickstart_validators = {
   hostname: [
-    { ops: { pattern: /^[a-z]([a-z0-9-.]){4,51}$/ }, error_message: 'Hostname must be alphanumeric with a length between 5 and 52 characters. Special characters are not allowed with the exception of dashes (IE -).', validatorFn: 'pattern' },
+    { ops: { pattern: /^[a-z]([a-z0-9-.]){4,39}$/ }, error_message: 'Hostname must be alphanumeric, less than 40 characters. Special characters are not allowed with the exception of dashes (IE -).', validatorFn: 'pattern' },
     { error_message: (value) => `Duplicate hostnames found: ${value}. Node must have a unique hostnames.`, validatorFn: 'unique' },
     { error_message: 'Hostname is required', validatorFn: 'required' }
   ],
@@ -205,7 +205,7 @@ export const kickstart_validators = {
   //general form validation
   controller_interface: [{ error_message: 'Controller Interface is required', validatorFn: 'required' }],
   domain: [
-    { ops: { pattern: /^[a-z]([a-z0-9-]){2,51}$/ }, error_message: 'Domain must be alphanumeric and 52 characters or less. Special characters are not allowed with the exception of dashes (IE -).', validatorFn: 'pattern' },
+    { ops: { pattern: /^[a-z]([a-z0-9-]){2,39}$/ }, error_message: 'Domain must be alphanumeric, less than 40 characters. Special characters are not allowed with the exception of dashes (IE -).', validatorFn: 'pattern' },
     { error_message: 'Domain is required', validatorFn: 'required' }
   ],
 
