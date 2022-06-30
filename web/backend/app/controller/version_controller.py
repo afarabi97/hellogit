e12b0af38d9dee7a51bc2354cb4d2ed2a47f7b97
Kendra Acosta <kacosta@technicacorp.com>
2022-06-30 09:10:45 -0500

    (refactor) THISISCVAH-12753: Version Controller - Refactor
    - Add api documentation
    - Add unit tests
    - Add models
    - Standardize error handling
    
    Issue: https://jira.di2e.net/browse/THISISCVAH-12753

--- web/backend/app/controller/version_controller.py
+++ web/backend/app/controller/version_controller.py
@@ -1,14 +1,21 @@
-from app.models.version_information import VersionInformationModel
+from app.models.common import COMMON_ERROR_MESSAGE
+from app.models.version_information import VERSION_NS, VersionInformationModel
 from app.service.system_info_service import (get_build_date, get_commit_hash,
                                              get_version)
+from app.utils.logging import logger
 from app.utils.namespaces import VERSION_NS
 from flask_restx.resource import Resource
 
 
 @VERSION_NS.route('/information')
-class Version(Resource):
+class VersionApi(Resource):
 
     @VERSION_NS.doc(description="Gets software version data.")
-    @VERSION_NS.response(200, 'VersionInformationModel', VersionInformationModel.DTO)
+    @VERSION_NS.response(200, "VersionInformationModel", VersionInformationModel.DTO)
+    @VERSION_NS.response(500, "ErrorMessage", COMMON_ERROR_MESSAGE)
     def get(self) -> VersionInformationModel:
-        return {"version": get_version(), "build_date": get_build_date(), "commit_hash": get_commit_hash()}, 200
+        try:
+            return {"version": get_version(), "build_date": get_build_date(), "commit_hash": get_commit_hash()}, 200
+        except Exception as exception:
+            logger.exception(exception)
+            return {"error_message": str(exception)}, 500
---
+++ web/backend/tests/unit/controller/test_version_controller.py
@@ -0,0 +1,24 @@
+from flask.testing import FlaskClient
+from pytest_mock.plugin import MockerFixture
+
+
+def test_get_version(client: FlaskClient, mocker: MockerFixture):
+    version = "3.7.0"
+    build_date = "june 09, 2022"
+    commit_hash = "48d1ce15"
+    mocker.patch("app.controller.version_controller.get_version", return_value=version)
+    mocker.patch("app.controller.version_controller.get_build_date", return_value=build_date)
+    mocker.patch("app.controller.version_controller.get_commit_hash", return_value=commit_hash)
+    response = client.get("/api/version/information")
+    assert response.status_code == 200
+    assert response.json["version"] == version
+
+def test_get_version_500(client: FlaskClient, mocker: MockerFixture):
+    build_date = "june 09, 2022"
+    commit_hash = "48d1ce15"
+    mocker.patch("app.controller.version_controller.get_version", side_effect=Exception('mocked error'))
+    mocker.patch("app.controller.version_controller.get_build_date", return_value=build_date)
+    mocker.patch("app.controller.version_controller.get_commit_hash", return_value=commit_hash)
+    response = client.get("/api/version/information")
+    assert response.status_code == 500
+    assert response.json["error_message"]
